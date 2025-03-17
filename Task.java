41c5749966e4a44b85f21c4c041f2a05452b9fe9
praga6802 <praga.06.08@gmail.com>
2025-03-17 23:41:17 +0530

    added 2 files

---
+++ Task.java
@@ -0,0 +1,10 @@
+public class Task{
+public static void main(String[] args){
+int[][][] x= new int[2][][];
+x[0]=new int[3][];
+x[1]=new int[2][2];
+x[0][0]= new int[1];
+x[0][1]= new int[2];
+x[0][2]= new int[3];
+}
+} 
---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2{
+public static void main(String[] args){
+float[] f=new float[5];
+f[0]=1.2f;
+f[1]=1.3f;
+f[2]=2.4f;
+f[3]=2.5f;
+f[4]=3.14f;
+System.out.println(f[0]);
+System.out.println(f[1]);
+System.out.println(f[2]);
+System.out.println(f[3]);
+System.out.println(f[4]);
+}
+}
+
+
